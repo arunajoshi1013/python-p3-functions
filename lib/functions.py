ff2e3fd59b8cee29c92f6c59ea3edbe08b1b973f
Aruna  Joshi <arunajoshi1013@gmail.com>
2023-07-09 15:38:03 -0700

    Fix functions lab

--- lib/functions.py
+++ lib/functions.py
@@ -4,16 +4,14 @@ def greet_programmer():
     print("Hello, programmer!")
 
 def greet(name):
-    print(f"Hello, {name}")
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    print(f"Hello, {name}")
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
     return num1 + num2
 
 def halve(number):
-    if (isinstance(number, int)):
-        return number / 2
-    else:
-        return None
+    return number / 2
+
